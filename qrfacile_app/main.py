63237a5b566e9b50e2595f48b70784c8b1e2c748
acaporaso75-del <acaporaso75@gmail.com>
2026-07-28 06:40:16 +0200

    feat: register invitation acceptance and center routes

--- qrfacile_app/main.py
+++ qrfacile_app/main.py
@@ -106,6 +106,8 @@ include_router_safe("qrfacile_app.label_history_ui")
 # Safe collaboration and invitation mutations must be registered before legacy routes with the same paths.
 include_router_safe("qrfacile_app.collaboration_management_ui")
 include_router_safe("qrfacile_app.invitation_management_ui")
+include_router_safe("qrfacile_app.invitation_acceptance_ui")
+include_router_safe("qrfacile_app.invitation_center_ui")
 include_router_safe("qrfacile_app.label_acl_ui")
 include_router_safe("qrfacile_app.collaboration_access_ui")
 include_router_safe("qrfacile_app.wine_compliance_ui")
